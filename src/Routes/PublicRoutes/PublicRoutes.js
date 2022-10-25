5bf6853cd275f916ff39b87db1a47017368a261b
mdiibrahim <mdiibrahim549@gmail.com>
2022-10-25 17:59:39 +0600

    tailwind daisy ui and react router dom are intalled

---
+++ src/Routes/PublicRoutes/PublicRoutes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+const PublicRoutes = () => {
+    const router = createBrowserRouter([
+        
+    ])
+    return (
+        <div>
+          
+        </div>
+    );
+};
+
+export default PublicRoutes;
