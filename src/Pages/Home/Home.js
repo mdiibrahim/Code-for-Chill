e895286a921745a44c217fb510b7b871386fd5eb
mdiibrahim <mdiibrahim549@gmail.com>
2022-10-27 07:19:04 +0600

    homepage are added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 
 const Home = () => {
     return (
@@ -7,9 +8,9 @@ const Home = () => {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-outline">Get Started for free</button>
+                        <h1 className="mb-5 text-6xl font-bold">Hey Coders!!!</h1>
+                        <p className="mb-5">Wanna chilling with coding? Then join us. We are committed to learning the code in a efficient manner, effective way with enjoy.</p>
+                        <button className="btn btn-outline">Get Started for free <BsFillArrowRightCircleFill className='text-lg ml-3'/></button>
                     </div>
                 </div>
             </div>
