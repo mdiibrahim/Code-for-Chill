6fad5ab50f7f471f4a8916436b8033f6dd3f43e8
mdiibrahim <mdiibrahim549@gmail.com>
2022-10-27 06:54:15 +0600

    login form are added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            <div className="hero min-h-screen bg-cover" style={{ backgroundImage: `url("background.jpg")` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-outline">Get Started for free</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,55 @@
 import React from 'react';
-
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import { BsGithub } from 'react-icons/bs';
 const Login = () => {
+    const [user, setUser] = useState(null);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        
+    }
     return (
         <div>
-            <h2>login</h2>
+            <div className="hero mx-auto my-20 w-9/12 bg-amber-700">
+                <div className="hero-content flex-col lg:flex-row-reverse lg:justify-between">
+                    <form onSubmit={handleSubmit} className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" name='email' />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" name='password' />
+                                <label className="label">
+                                    <small>Not registered? <Link to='/register' className='link link-primary'>Register now</Link> </small>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn  btn-outline">Sign In</button>
+                            </div>
+                            <div className="form-control mt-1">
+                                <button className="btn  btn-outline btn-success"><FcGoogle className='text-2xl mr-2'/> Sign In with Google</button>
+                            </div>
+                            <div className="form-control mt-1">
+                                <button className="btn btn-outline"><BsGithub className='text-2xl mr-2'/>Sign In with Github</button>
+                            </div>
+                        </div>
+                    </form>
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-xl font-medium ">Sign in <br /><strong className='text-3xl font-bold'>Code for Chill</strong></h1>
+                        <p className="py-6">Learning the code with feel and chill</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
