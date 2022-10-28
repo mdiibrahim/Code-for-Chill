8a4e15aac97124a2a1e1fa9da23f9a16580bd902
mdiibrahim <mdiibrahim549@gmail.com>
2022-10-28 21:41:26 +0600

    finished

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import { BsGithub } from 'react-icons/bs';
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useContext } from 'react';
-import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
